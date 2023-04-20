c55aa96581688653fdb8ab22d0298af39ce7d22d
wl-hsu <wlhsu.github@gmail.com>
2023-04-20 14:03:01 -0400

    update

--- assignment02/Practice Strings/2.SplitString/SplitString/SplitString/Program.cs
+++ assignment02/Practice Strings/2.SplitString/SplitString/SplitString/Program.cs
@@ -5,23 +5,13 @@ class Program
     public static void Main()
     {
         string test = "C# is not C++, and PHP is not Delphi!";
-        string[] words = test.Split(new char[] { ' ', '.', ',', ':', ';', '=', '(', ')', '&', '[', ']', '"', '\'', '\\', '/', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-        foreach(string word in words)
-        {
-            Console.WriteLine(word);
-        }
-    }
-
-    public static string ReverseWords (String s)
-    {
-        string[] words = s.Split(new char[] { ' ' });
-        Array.Reverse(words);
-        string ans = string.Join(" ", words);
-        return ans;
+        //string[] words = test.Split(" ");
+        String words = ReverseWords(test);
+        Console.WriteLine(words);
 
     }
 
-    public static string ReverseWords1(string str)
+    public static string ReverseWords(string str)
     {
         StringBuilder answer = new StringBuilder();
         string[] words = str.Split('.', ' ');
@@ -38,8 +28,5 @@ class Program
         return answer.ToString();
     }
 
-    public static string ReverseWords2(string str)
-    {
-        return String.Join(" ", str.Split('.', ' ').Reverse()).ToString();
-    }
+
 }
