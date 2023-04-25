369625dbaa8b90e19b917edda8ba5158578ca56b
wl-hsu <wlhsu.github@gmail.com>
2023-04-25 12:34:49 -0400

    update

---
+++ assignment04/CStack/CStack/CStack/MyStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CStack
+{
+    class MyStack<T>
+    {
+        private List<T> items = new List<T>();
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            T item = items[Count - 1];
+            items.RemoveAt(Count - 1);
+            return item;
+        }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+    }
+}
---
+++ assignment04/CStack/CStack/CStack/Program.cs
@@ -0,0 +1,7 @@
+﻿using CStack;
+
+MyStack<int> intStack1 = new MyStack<int>();
+MyStack<double> doubletack = new MyStack<double>();
+MyStack<Decimal> decimalStack2 = new MyStack<Decimal>();
+MyStack<Object> objectStack2 = new MyStack<Object>();
+MyStack<string> stringStack = new MyStack<string>();
