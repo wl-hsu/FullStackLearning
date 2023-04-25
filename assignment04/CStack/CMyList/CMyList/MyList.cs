e326427af61ce522fbd49b44f752c44cbbda088c
wl-hsu <wlhsu.github@gmail.com>
2023-04-25 12:48:39 -0400

    update

---
+++ assignment04/CStack/CMyList/CMyList/MyList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMyList
+{
+    class MyList<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+
+        public T Remove(int index)
+        {
+            if (index < 0 || index >= items.Count)
+            {
+                throw new IndexOutOfRangeException("Index Null Exception");
+            }
+
+            T element = items[index];
+            items.RemoveAt(index);
+            return element;
+        }
+
+        public bool Contains(T element)
+        {
+            return items.Contains(element);
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public void InsertAt(T element, int index)
+        {
+            if (index < 0 || index > items.Count)
+            {
+                throw new IndexOutOfRangeException("Index Null Exception");
+            }
+
+            items.Insert(index, element);
+        }
+
+        public void DeleteAt(int index)
+        {
+            if (index < 0 || index >= items.Count)
+            {
+                throw new IndexOutOfRangeException("Index Null Exception");
+            }
+
+            items.RemoveAt(index);
+        }
+
+        public T Find(int index)
+        {
+            if (index < 0 || index >= items.Count)
+            {
+                throw new IndexOutOfRangeException("Index Null Exception");
+            }
+
+            return items[index];
+        }
+    }
+}
---
+++ assignment04/CStack/CMyList/CMyList/Program.cs
@@ -0,0 +1,6 @@
+﻿using CMyList;
+using System.Collections.Generic;
+
+MyList<int> intList = new MyList<int>();
+MyList<Object> objectList = new MyList<Object>();
+MyList<string> stringList = new MyList<string>();
