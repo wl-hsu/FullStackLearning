e9d1a169ffd0e3a637a9eeda037e58992231b278
wl-hsu <wlhsu.github@gmail.com>
2023-05-03 06:34:34 -0400

    update

---
+++ assignment04/CRUD/CRUD/CRUD/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    public class GenericRepository<T> : IRepository<T>, IGenericRepository<T> where T : class, IEntity
+    {
+        private readonly DbContext dbContext;
+        private readonly DbSet<T> dbSet;
+
+        public GenericRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = dbContext.Set<T>();
+        }
+
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return dbSet.SingleOrDefault(e => e.Id == id);
+        }
+    }
+}
---
+++ assignment04/CRUD/CRUD/CRUD/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CRUD
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
